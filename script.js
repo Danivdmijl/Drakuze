07604413f61966c1e537fb0e027b5d303ab2ab39
Dani <Danivdmijl@outlook.com>
2022-03-24 15:22:30 +0100

    ja

---
+++ script.js
@@ -0,0 +1,37 @@
+const pokemonImage = document.getElementById("js--pokemon-image");
+let randomNumber = Math.floor(Math.random() * 897 + 1);
+
+
+let pokemon = fetch("https://pokeapi.co/api/v2/pokemon/" + randomNumber)
+.then(function(response){
+    return response.json();
+})
+.then(function(realData){
+    console.log(realData);
+    pokemonImage.src = realData.sprites.front_default;
+});
+
+
+const catchButton = document.getElementById("js--catch-button");
+const pokemonText = document.getElementById("js--pokemon-text");
+let pokemonGamePlayed = false;
+
+function reloadPage() {
+    window.location.reload();
+}
+
+catchButton.onclick = function(){
+    if(pokemonGamePlayed === false){
+        let catchNumber = Math.floor(Math.random() * 2);
+        if(catchNumber === 0){
+            pokemonText.innerText = "Pokemon Fled!"
+            setTimeout(reloadPage, 2000);
+        }
+    
+        else{
+            pokemonText.innerText = "Pokemon Caught!"
+            setTimeout(reloadPage, 2000);
+        }
+        pokemonGamePlayed = true
+    }
+}
