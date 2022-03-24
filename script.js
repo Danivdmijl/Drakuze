7364bf8fd0f1092adbcb62595df21ab2b5ff9cce
Dani <Danivdmijl@outlook.com>
2022-03-24 23:04:11 +0100

    reload

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const pokemonImage = document.getElementById("js--pokemon-image");
 let randomNumber = Math.floor(Math.random() * 897 + 1);
 
+console.log(randomNumber)
+
 
 let pokemon = fetch("https://pokeapi.co/api/v2/pokemon/" + randomNumber)
 .then(function(response){
@@ -16,22 +18,44 @@ const catchButton = document.getElementById("js--catch-button");
 const pokemonText = document.getElementById("js--pokemon-text");
 let pokemonGamePlayed = false;
 
-function reloadPage() {
-    window.location.reload();
-}
 
 catchButton.onclick = function(){
     if(pokemonGamePlayed === false){
         let catchNumber = Math.floor(Math.random() * 2);
         if(catchNumber === 0){
             pokemonText.innerText = "Pokemon Fled!"
-            setTimeout(reloadPage, 1200);
+            randomNumber = Math.floor(Math.random() * 897 + 1);
+            console.log(randomNumber);
+            fetch("https://pokeapi.co/api/v2/pokemon/" + randomNumber)
+            .then(function(response){
+                return response.json();
+            })
+            .then(function(realData){
+                console.log(realData);
+                pokemonImage.src = realData.sprites.front_default;
+                setTimeout(nieuwepokemon, 750);
+            });            
         }
     
         else{
             pokemonText.innerText = "Pokemon Caught!"
-            setTimeout(reloadPage, 1200);
+            randomNumber = Math.floor(Math.random() * 897 + 1);
+            console.log(randomNumber);
+            fetch("https://pokeapi.co/api/v2/pokemon/" + randomNumber)
+            .then(function(response){
+                return response.json();
+            })
+            .then(function(realData){
+                console.log(realData);
+                pokemonImage.src = realData.sprites.front_default;
+                setTimeout(nieuwepokemon, 750);
+            });  
         }
         pokemonGamePlayed = true
     }
+}
+
+function nieuwepokemon(){
+    pokemonText.innerText = "A wild Pokemon appeared!"
+    pokemonGamePlayed = false
 }
